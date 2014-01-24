2b51891393d35140be2262bb0ec87b0a80f5831c
Brian McKenna <brian@simpleenergy.com>
2014-01-23 21:49:28 -0700

    Make program disable all debug settings
    Just so we don't rely on getting settings to get shadows back.

--- main.c
+++ main.c
@@ -9,9 +9,7 @@ extern void CGSGetDebugOptions(CGSDebugOptions *options);
 extern void CGSSetDebugOptions(CGSDebugOptions options);
 
 int main(int argc, char *argv[]) {
-    CGSDebugOptions options;
-    CGSGetDebugOptions(&options);
-    options ^= kCGSDebugOptionNoShadows;
+    CGSDebugOptions options = kCGSDebugOptionNone;
     CGSSetDebugOptions(options);
     return 0;
 }
